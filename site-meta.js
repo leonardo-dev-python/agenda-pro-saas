1925d33630cabc303822dfe3a98e00b4674bd666
Leona <owner@agendapro.local>
2026-05-02 18:20:59 -0300

    Update public support email to Hostinger address

--- site-meta.js
+++ site-meta.js
@@ -1,8 +1,8 @@
 window.AGENDA_PRO_SITE_META = {
   brandName: "Agenda Pro",
-  supportEmail: "suporte@agendapro.com.br",
-  billingEmail: "financeiro@agendapro.com.br",
-  privacyEmail: "privacidade@agendapro.com.br",
+  supportEmail: "contato@lrtechsolutions.com",
+  billingEmail: "contato@lrtechsolutions.com",
+  privacyEmail: "contato@lrtechsolutions.com",
   supportWhatsAppLabel: "Atendimento comercial sob demanda",
   legalNotice: "Dados societários e e-mails oficiais podem ser atualizados a qualquer momento pelos canais oficiais do Agenda Pro.",
 };
